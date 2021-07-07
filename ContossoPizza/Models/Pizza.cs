3745687f7f0bcd2eee2887b16d901ef8ca7d78a7
RejinaldWild <68392463+RejinaldWild@users.noreply.github.com>
2021-07-07 06:19:17 +0300

    Added ASP.Net Core Tutorial by MSDN

---
+++ ContossoPizza/Models/Pizza.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ContossoPizza.Models;
+using ContossoPizza;
+
+namespace ContossoPizza.Models
+{
+	public class Pizza
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public bool IsGlutenFree { get; set; }
+	}
+
+}
+
+
